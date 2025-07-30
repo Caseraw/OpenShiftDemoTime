6a266140fc7a6945726efeca739440aeacbbaebb
Kasra <kasra.amirsarvari@gmail.com>
2025-07-30 09:09:48 +0000

    Add changes

--- scenarios/demo-open-hybrid-cloud/demo-initialize.sh
+++ scenarios/demo-open-hybrid-cloud/demo-initialize.sh
@@ -3,7 +3,7 @@
 # Define paths
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
 COMPONENTS_BASE="$PROJECT_DIR/assets/components"
-SCENARIO_BASE="$PROJECT_DIR/scenarios/demo-rhacm-hybrid-workload"
+SCENARIO_BASE="$PROJECT_DIR/scenarios/demo-open-hybrid-cloud"
 
 # Load additional functions
 source "$PROJECT_DIR/assets/scripts/shell/lib/show_msg.sh"
