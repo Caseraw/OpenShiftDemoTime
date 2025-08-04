366cde91da0116613ba968ba0e7bffa9a6a34667
Kasra <kasra.amirsarvari@gmail.com>
2025-08-04 11:24:31 +0000

    Add changes

--- scenarios/uc22-cluster-upgrade/backend/app.py
+++ scenarios/uc22-cluster-upgrade/backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify
 import os
 from datetime import datetime
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/status')
 def status():
