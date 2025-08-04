f9766d32324b201a375bab988781fd6bd84946b5
wael2000 <waelibrahim2000@hotmail.com>
2025-08-04 19:05:13 +0000

    Done

--- scenarios/uc04-container-as-a-service/app/src/test/java/org/acme/GreetingResourceTest.java
+++ scenarios/uc04-container-as-a-service/app/src/test/java/org/acme/GreetingResourceTest.java
@@ -5,16 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @QuarkusTest
 class GreetingResourceTest {
+
+     @ConfigProperty(name = "BUILD")
+    String build;
+
     @Test
     void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .body(is("Hello World, container image from [ " + build + " ]"));
     }
 
 }
