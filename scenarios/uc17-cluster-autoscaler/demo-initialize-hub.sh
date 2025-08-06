3a91c8e1fddcef75ecda06a4a89aa60fe94812c5
Kasra <kasra.amirsarvari@gmail.com>
2025-08-06 15:34:28 +0000

    add changes

--- scenarios/uc17-cluster-autoscaler/demo-initialize-hub.sh
+++ scenarios/uc17-cluster-autoscaler/demo-initialize-hub.sh
@@ -10,7 +10,16 @@ source "$PROJECT_DIR/assets/scripts/shell/lib/show_msg.sh"
 source "$PROJECT_DIR/assets/scripts/shell/lib/show_divider.sh"
 source "$PROJECT_DIR/assets/scripts/shell/lib/run_cmd.sh"
 
-# 0) Deploy the first ArgoCD Applications to initiate with
+# 0) Copy the AWS Credentials to use in demo-pipelines
+SCRIPT="$COMPONENTS_BASE/openshift-rhacm-credentials-aws/copy-secret-for-demo-pipelines.sh"
+
+show_msg "show-date" "INFO" "Bootstrap - Copy AWS Credentials for demo-pipelines" "⏳"
+show_msg "show-date" "INFO" "Script" "$SCRIPT"
+source "$SCRIPT"
+show_msg "show-date" "INFO" "Bootstrap - Copy AWS Credentials for demo-pipelines" "Completed" "✅"
+
+
+# 1) Deploy the first ArgoCD Applications to initiate with
 show_msg "show-date" "INFO" "Bootstrap - Deploy the first ArgoCD Applications to initiate" "⏳"
 
 KUSTOMIZE_BASE="argocd-applications/bootstrap"
