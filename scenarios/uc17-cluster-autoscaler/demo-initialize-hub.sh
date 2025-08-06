6364d75a06f19e44d929f849dd38ec3bccc1f1ad
Kasra <kasra.amirsarvari@gmail.com>
2025-08-06 15:30:47 +0000

    add changes

--- scenarios/uc17-cluster-autoscaler/demo-initialize-hub.sh
+++ scenarios/uc17-cluster-autoscaler/demo-initialize-hub.sh
@@ -18,5 +18,6 @@ show_msg "show-date" "INFO" "Running Kustomize build..."
 kustomize build "$SCENARIO_BASE/argocd-applications/bootstrap-spoke" | oc apply -f -
 kustomize build "$SCENARIO_BASE/argocd-applications/demo-pipelines-hub" | oc apply -f -
 kustomize build "$SCENARIO_BASE/argocd-applications/demo-pipelines-spoke" | oc apply -f -
+kustomize build "$SCENARIO_BASE/argocd-applications/stress-job" | oc apply -f -
 
 show_msg "show-date" "INFO" "Bootstrap - Deploy the first ArgoCD Applications to initiate" "Completed" "✅"
