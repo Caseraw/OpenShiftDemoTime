fbf1f17c169d619ef8d9146483f771e190594246
wael2000 <waelibrahim2000@hotmail.com>
2025-08-13 21:21:34 +0000

    Done

--- scenarios/uc16-workload-scalability/stress.sh
+++ scenarios/uc16-workload-scalability/stress.sh
@@ -1,1 +1,3 @@
+#ab -n 200 -c 20 -v 2  https://scaling-scaling.apps.cluster-sfhmt.sfhmt.sandbox587.opentlc.com/
+
 ab -n 500 -c 100 -v 2  https://scaling-scaling.apps.cluster-sfhmt.sfhmt.sandbox587.opentlc.com/
