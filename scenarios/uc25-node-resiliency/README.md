12f70e91979cfaf8eccd8786594cc13cb0f42072
Kasra <kasra.amirsarvari@gmail.com>
2025-08-13 17:29:11 +0000

    changes

---
+++ scenarios/uc25-node-resiliency/README.md
@@ -0,0 +1,18 @@
+# Demo - Use Case 25 - Node resiliency
+
+This scenario covers the exploration of Worker Node and Application workload
+behaviour during Node maintenance or possibly even Node outages.
+
+## Prerequisites
+
+### UC19 Network Graphs
+
+Everything relies on the environment set up like the scenario [UC19 Network
+Graphs](../uc19-network-graphs/README.md). For more on the setup please checkout
+the instructions.
+
+## Demo storyline
+
+- Make a Worker Node unschedulable (Webconsole and CLI)
+- Drain a Worker Node (CLI)
+- 
---
+++ scenarios/uc25-node-resiliency/bootstrap-virt-cluster.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Define paths
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
+COMPONENTS_BASE="$PROJECT_DIR/assets/components"
+SCENARIO_BASE="$PROJECT_DIR/scenarios/uc25-node-resiliency"
+
+# Load additional functions
+source "$PROJECT_DIR/assets/scripts/shell/lib/show_msg.sh"
+source "$PROJECT_DIR/assets/scripts/shell/lib/show_divider.sh"
+source "$PROJECT_DIR/assets/scripts/shell/lib/run_cmd.sh"
+
+# The expectation here is that we have some prerequisites set up:
+# - Running OpenShift Cluster
+# - Privileged user with access to the Cluster
+# - Proper working environment and tools as stated in the VSCode .devcontainer
+# - Required credentials for the AWS account that we will be using
+
+# 0) Login to the OpenShift Cluster
+SCRIPT="$COMPONENTS_BASE/functions/login-on-openshift.sh"
+
+show_msg "show-date" "INFO" "Bootstrap - Login to OpenShift" "⏳"
+show_msg "show-date" "INFO" "Script" "$SCRIPT"
+source "$SCRIPT"
+show_msg "show-date" "INFO" "Bootstrap - Login to OpenShift" "Completed" "✅"
+
+# 1) Install the OpenShift Local Storage Operator
+SCRIPT="$COMPONENTS_BASE/openshift-local-storage/setup.sh"
+
+show_msg "show-date" "INFO" "Bootstrap - Install OpenShift Local Storage Operator" "⏳"
+show_msg "show-date" "INFO" "Script" "$SCRIPT"
+source "$SCRIPT"
+show_msg "show-date" "INFO" "Bootstrap - Install OpenShift Local Storage Operator" "Completed" "✅"
+
+# 2) Install the OpenShift Data Foundation Operator
+SCRIPT="$COMPONENTS_BASE/openshift-storage/setup.sh"
+
+show_msg "show-date" "INFO" "Bootstrap - Install OpenShift Data Foundation Operator" "⏳"
+show_msg "show-date" "INFO" "Script" "$SCRIPT"
+source "$SCRIPT"
+show_msg "show-date" "INFO" "Bootstrap - Install OpenShift Data Foundation Operator" "Completed" "✅"
+
+# 3) Install the OpenShift Virtualization Operator
+SCRIPT="$COMPONENTS_BASE/openshift-virtualization/setup.sh"
+
+show_msg "show-date" "INFO" "Bootstrap - Install OpenShift Virtualization Operator" "⏳"
+show_msg "show-date" "INFO" "Script" "$SCRIPT"
+source "$SCRIPT"
+show_msg "show-date" "INFO" "Bootstrap - Install OpenShift Virtualization Operator" "Completed" "✅"
+
+# 4) Install the OpenShift Serverless Operator
+SCRIPT="$COMPONENTS_BASE/openshift-serverless/setup.sh"
+
+show_msg "show-date" "INFO" "Bootstrap - Install OpenShift Serverless Operator" "⏳"
+show_msg "show-date" "INFO" "Script" "$SCRIPT"
+source "$SCRIPT"
+show_msg "show-date" "INFO" "Bootstrap - Install OpenShift Serverless Operator" "Completed" "✅"
+
+# 5) Deploy the first ArgoCD Applications to initiate with
+show_msg "show-date" "INFO" "Bootstrap - Deploy the first ArgoCD Applications to initiate" "⏳"
+
+show_msg "show-date" "INFO" "Running Kustomize build..."
+kustomize build "$SCENARIO_BASE/argocd-applications/hybrid-workload" | oc apply -f -
+
+show_msg "show-date" "INFO" "Bootstrap - Deploy the first ArgoCD Applications to initiate" "Completed" "✅"
