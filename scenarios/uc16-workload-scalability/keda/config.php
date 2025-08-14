062df6ac7c8cf5966cf4ad4a5e8b7bab7ef15471
wael2000 <waelibrahim2000@hotmail.com>
2025-08-14 06:13:08 +0000

    done

--- scenarios/uc16-workload-scalability/keda/config.php
+++ scenarios/uc16-workload-scalability/keda/config.php
@@ -9,7 +9,7 @@ if($link === false){
 $ddl_sql = "CREATE TABLE IF NOT EXISTS request_counter ( id INT PRIMARY KEY AUTO_INCREMENT, count INT NOT NULL DEFAULT 0);";
 mysqli_query($link, $ddl_sql);
 
-$result = $mysqli->query("SELECT count FROM request_counter WHERE id = 1");
+$result = $link->query("SELECT count FROM request_counter WHERE id = 1");
 $row = $result->fetch_assoc();
 if ($row === null) {
     mysqli_query($link,"INSERT INTO request_counter (count) VALUES (0);");
